e5c17ec72e754226879dce2027617f4f3b5e3c12
Mickly <maykiqq@gmail.com>
2023-04-06 19:18:07 +0300

    finished layout

---
+++ script.js
@@ -0,0 +1,26 @@
+"use strict"
+const questions =[
+    {
+        question: "What is the capital of France?",
+        options: ["New York","London","Paris","Dublin"],
+        answer:"Paris"
+    },
+    {
+        question: "What is the capital of Canada?",
+        options: ["Toronto","Ottawa","Vancouver","Dublin"],
+        answer:"Ottawa"
+    },
+    {
+        question: "What is the longest river in the world?",
+        options: ["Nile","Amazon","Yangtze","Volga"],
+        answer:"Nile"
+    },
+    {
+        question: "How many chromosomes are in the human genome? ",
+        options: ["42","44","46","47"],
+        answer:"Paris"
+    },
+    
+] 
+
+let currentQuestionIndex = 0;
